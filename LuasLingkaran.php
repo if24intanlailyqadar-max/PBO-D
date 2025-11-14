712fb0ba93ae02b2689456a8c360963801688eb1
if24intanlailyqadar-max <if24.intanlailyqadar@mhs.ubpkarawang.ac.id>
2025-11-14 18:34:39 +0700

    feat: namespace

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,17 +1,19 @@
 <?php
 
-class LuasLingkaran{
+namespace App\Math;
+
+class LuasLingkaran {
 
     public const phi = 3.14;
-    public int $jari;
-
-    public function __construct($isijari = 1) {
-        $this->jari = $isijari;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
     }
 
-    public function tampil($nama ='ban')  {
-        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "Lingkaran {$nama} hasilnya adalah: ". $rumus;
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing() {
@@ -24,7 +26,3 @@ class LuasLingkaran{
         echo "udah ah cape";
     }
 }
-
-$lingkaran = new LuasLingkaran(7);
-$lingkaran->tampil('roda'); //panggil method
-LuasLingkaran::testing();
